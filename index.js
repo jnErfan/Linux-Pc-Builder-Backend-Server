ff611ae4a597525355f176855f066b5f69840d0f
jnErfan <j.n.erfan420@gmail.com>
2021-11-10 01:26:15 +0600

    Heroku Problem

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 
 // Run Test
-app.get("/", (req, res) => {
-  res.send(
-    `<h1  style="text-align: center; margin-top:100px;  font-weight: 900; color: blue">Welcome To Linux Pc Builder Server</h1>`
-  );
-});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qyw7u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
