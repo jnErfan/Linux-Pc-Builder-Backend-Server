e8e7f19fc681b3fdf5273f1f125a300aa8c9b010
jnErfan <j.n.erfan420@gmail.com>
2021-11-12 17:51:25 +0600

    Get All Collection

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
 
 // Run Test
 app.get("/", (req, res) => {
@@ -18,9 +22,53 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 client.connect((err) => {
-  const database = client.db("test");
-  const computerCollections = database.collection("computers");
-  console.log("Connection Successful");
+  const database = client.db("linux_pc_builder");
+  const desktopCollections = database.collection("desktops");
+  const usersCollection = database.collection("users");
+
+  // Get All Desktop Collection
+  app.get("/desktopsPagination", async (req, res) => {
+    const desktopShowUi = await desktopCollections.find({});
+    const page = req.query.page;
+    const size = parseInt(req.query.size);
+    let desktopPackage;
+    const count = await desktopShowUi.count();
+    if (page) {
+      desktopPackage = await desktopShowUi
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+    } else {
+      desktopPackage = await desktopShowUi.toArray();
+    }
+    res.send({ count, desktopPackage });
+  });
+
+  app.get("/desktopDetails/:id", async (req, res) => {
+    const params = req.params.id;
+    const query = { _id: ObjectId(params) };
+    const result = await desktopCollections.find(query).toArray();
+    res.send(result);
+  });
+
+  // Create Email Password Info
+  app.post("/users", async (req, res) => {
+    const user = req.body;
+    const result = await usersCollection.insertOne(user);
+    res.json(result);
+  });
+
+  // Google Facebook Github Info
+  app.put("/users", async (req, res) => {
+    const user = req.body;
+    const query = { email: user.email };
+    const options = { upsert: true };
+    const updateDoc = { $set: user };
+    const result = await usersCollection.updateOne(query, updateDoc, options);
+    res.json(result);
+    console.log(result);
+  });
+
   // client.close();
 });
 
