1afbaab1ae572f96850f6e69485c8ff29ba59b6d
jnErfan <j.n.erfan420@gmail.com>
2021-11-13 23:12:16 +0600

    Assignment 12 Finished

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.get("/", (req, res) => {
   );
 });
 
-// https://linux-pc-builder-server.herokuapp.com/
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qyw7u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
